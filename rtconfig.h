5ac4ad8067b7fc1ba4425b2af1e07e16be8ee1e9
KunYi Chen <kunyi.chen@gmail.com>
2024-10-25 14:59:50 +0800

    enabled sdcard support

--- rtconfig.h
+++ rtconfig.h
@@ -76,6 +76,30 @@
 
 /* DFS: device virtual file system */
 
+#define RT_USING_DFS
+#define DFS_USING_POSIX
+#define DFS_USING_WORKDIR
+#define DFS_FD_MAX 16
+#define RT_USING_DFS_V1
+#define DFS_FILESYSTEMS_MAX 4
+#define DFS_FILESYSTEM_TYPES_MAX 4
+#define RT_USING_DFS_ELMFAT
+
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_3
+#define RT_DFS_ELM_USE_LFN 3
+#define RT_DFS_ELM_LFN_UNICODE_0
+#define RT_DFS_ELM_LFN_UNICODE 0
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 512
+#define RT_DFS_ELM_REENTRANT
+#define RT_DFS_ELM_MUTEX_TIMEOUT 3000
+/* end of elm-chan's FatFs, Generic FAT Filesystem Module */
+#define RT_USING_DFS_DEVFS
 /* end of DFS: device virtual file system */
 
 /* Device Drivers */
@@ -88,6 +112,15 @@
 #define RT_USING_I2C
 #define RT_USING_I2C_BITOPS
 #define RT_USING_PWM
+#define RT_USING_RTC
+#define RT_USING_SDIO
+#define RT_SDIO_STACK_SIZE 512
+#define RT_SDIO_THREAD_PRIORITY 15
+#define RT_MMCSD_STACK_SIZE 1024
+#define RT_MMCSD_THREAD_PREORITY 22
+#define RT_MMCSD_MAX_PARTITION 16
+#define RT_USING_SPI
+#define RT_USING_SPI_MSD
 #define RT_USING_TOUCH
 #define RT_TOUCH_PIN_IRQ
 #define RT_USING_PIN
@@ -112,6 +145,7 @@
 
 /* POSIX (Portable Operating System Interface) layer */
 
+#define RT_USING_POSIX_DEVIO
 
 /* Interprocess Communication (IPC) */
 
@@ -342,13 +376,20 @@
 #define BSP_USING_UART9
 #define BSP_UART9_RX_BUFSIZE 256
 #define BSP_UART9_TX_BUFSIZE 0
+#define BSP_USING_SCI_SPI6
+#define BSP_USING_SPI
 #define BSP_USING_I2C
 #define BSP_USING_I2C1
 #define BSP_I2C1_SCL_PIN 0x0202
 #define BSP_I2C1_SDA_PIN 0x0203
+#define BSP_USING_FS
+#define BSP_USING_SDCARD_FS
+#define BSP_USING_SPICARD_FS
+#define BSP_USING_SDHI
 #define BSP_USING_PWM
 #define BSP_USING_PWM5
 #define BSP_USING_LCD
+#define BSP_USING_JPEG
 #define BSP_USING_LVGL
 #define BSP_USING_LCD_RGB
 #define BSP_USING_TOUCH
