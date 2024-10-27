6a42b25f9b7676ae05553cc9f8f080fea679190f
KunYi Chen <kunyi.chen@gmail.com>
2024-10-27 09:21:03 +0800

    import lv_rajpg

---
+++ board/lv_rajpg/lv_rajpg.h
@@ -0,0 +1,43 @@
+/**
+ * @file lv_rajpg.h
+ *
+ */
+
+#ifndef LV_RAJPEG_H
+#define LV_RAJPEG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#if LV_USE_RAJPG
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void lv_rajpeg_init(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*LV_USE_RAJPG*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LV_RAJPEG_H */
