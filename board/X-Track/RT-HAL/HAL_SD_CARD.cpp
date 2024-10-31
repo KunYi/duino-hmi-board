0bf2733e1a21779d658269f4f59d04e8d58d1150
KunYi Chen <kunyi.chen@gmail.com>
2024-10-31 18:19:28 +0800

    implement get capacity of SD card

--- board/X-Track/RT-HAL/HAL_SD_CARD.cpp
+++ board/X-Track/RT-HAL/HAL_SD_CARD.cpp
@@ -1,4 +1,6 @@
 ﻿#include "HAL.h"
+#include <rtthread.h>
+#include <rtdevice.h>
 
 bool HAL::SD_Init()
 {
@@ -12,7 +14,30 @@ bool HAL::SD_GetReady()
 
 float HAL::SD_GetCardSizeMB()
 {
-    return 32 * 1024;
+    rt_device_t sd = rt_device_find("sd0");
+    if( sd == RT_NULL) {
+        return 0.0f;
+    }
+
+    if (RT_EOK != rt_device_open(sd, RT_DEVICE_FLAG_RDONLY)) {
+        return 0.0f;
+    }
+
+    struct rt_device_blk_geometry geometry;
+    rt_memset(&geometry, 0, sizeof(geometry));
+    if (RT_EOK != rt_device_control(sd,
+                RT_DEVICE_CTRL_BLK_GETGEOME,
+                &geometry)) {
+        rt_device_close(sd);
+        return 0.0f;
+    }
+    rt_device_close(sd);
+    /*
+      return size, unit GiB
+      (1000*1000) because storage industry use 1000 not 1024
+    */
+    return ((geometry.sector_count * geometry.bytes_per_sector)/(1000*1000));
+
 }
 
 const char* HAL::SD_GetTypeName()
